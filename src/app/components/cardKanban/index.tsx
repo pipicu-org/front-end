bd6579ef4fb8fd61c0d6dfda8f5675f89eac34bf
Cristian Juarez <cristianjuarezdev@gmail.com>
2025-08-14 21:03:17 -0300

    Se renombra OrderCard y se ajusta CardKanban

--- src/app/components/cardKanban/index.tsx
+++ src/app/components/cardKanban/index.tsx
@@ -1,11 +1,12 @@
 "use client";
-import Orden from "../orden/index";
+
+import OrderCard from "../cards";
 
 const CardKanban = () => {
 
     const estilo = {
-        background: 
-        `linear-gradient
+        background:
+            `linear-gradient
         (135deg, 
         rgba(161,161,161,0.5) 0%,
         rgba(161,161,161,0.05) 66%,
@@ -13,8 +14,7 @@ const CardKanban = () => {
         #ffffff`
     }
     return (
-        <>
-        <div className="rounded-xl p-3 bg-[#290D1B0D] mt-5">
+        <div className="rounded-xl p-3 bg-[#290D1B0D]">
             {/* ~~~ Header ~~~ */}
             <div className="flex items-center justify-between">
                 <div className="inline-flex items-center gap-2">
@@ -22,26 +22,19 @@ const CardKanban = () => {
                     <div className="w-[5px] h-[5px] bg-white rounded-full shadow-[0_0_10px_5px_rgba(255,255,255,1)]"></div>
                     <h2 className="font-bold">Creados</h2>
                 </div>
-                
                 <span className="font-bold">3</span>
             </div>
-            
+
             {/* ~~~ Row ~~~ */}
             <div className="mt-5 flex gap-5  overflow-hidden rounded-xl">
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
-                <Orden/>
+                {
+                    // Mock filler
+                    [1, 2, 3, 4, 5].map((i) =>
+                        <OrderCard key={i} />
+                    )
+                }
             </div>
         </div>
-        </>
     )
 }
 
