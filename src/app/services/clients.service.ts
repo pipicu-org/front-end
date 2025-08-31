686f2545dc5291086ece217c629906b4859a49b1
Cristian Juarez <cristianjuarezdev@gmail.com>
2025-08-31 15:56:41 -0300

    Added service

---
+++ src/app/services/clients.service.ts
@@ -0,0 +1,46 @@
+import api from "./api";
+
+// Crear cliente
+export async function createClient(client: IClient) {
+  const { data } = await api.post("/client", client);
+  return data;
+}
+
+// Obtener cliente por id
+interface IGetClient {
+  data: IClient
+}
+
+export async function getClientById(id: number): Promise<IGetClient> {
+  const { data }: { data: IGetClient } = await api.get(`/client/${id}`);
+  return data;
+}
+
+// Actualizar cliente
+export async function updateClient(id: number, client: Partial<IClient>) {
+  const { data } = await api.patch(`/client/${id}`, client);
+  return data;
+}
+
+// Buscar clientes con paginación
+interface IGetClients {
+  data: IClient[]
+  limit: number
+  page: number
+  search: string
+  total: number
+}
+
+export async function searchClients(search: string, page: number = 1): Promise<IGetClients> {
+  const { data }: { data: IGetClients } = await api.get(`/client`, {
+    params: { search, page },
+  });
+
+  return data;
+}
+
+// Eliminar cliente
+export async function deleteClient(id: number) {
+  const { data } = await api.delete(`/client/${id}`);
+  return data;
+}
