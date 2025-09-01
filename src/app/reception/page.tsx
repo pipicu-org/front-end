bf6c876bbda10bda8685e99407375a6d5b3b4be1
LautaroRiofrio <riofriolautaro@gmail.com>
2025-09-01 14:46:02 -0300

    Merge branch 'componentes' into primera-version

--- src/app/reception/page.tsx
+++ src/app/reception/page.tsx
@@ -1,10 +1,33 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Summary from "./components/summary/Summary";
 import { OrderModal } from "./components/orden";
 
+import { getOrdersByStateID } from "../services/order.service";
+
 const Reception = () => {
+
+    interface IGetOrders {
+        search: string
+        total: number
+        page: number
+        limit: number
+        data: IOrder[]
+    }
+    const [orders, setOrders] = useState<IGetOrders>();
+    useEffect(() => {
+        getOrdersByStateID("1").then(orderResponse => {
+            setOrders(orderResponse);
+        }).catch(console.error)
+
+    }, []);
+
+
+    useEffect(() => {
+        console.log("ordenes: ", orders);
+
+    }, [orders])
     const ordenes_creados: IOrder[] = [];
     const ordenes_pendientes: IOrder[] = [];
     const ordenes_preparados: IOrder[] = [];
