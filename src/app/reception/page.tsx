4bf06dc79ae1e9dc45b33bf241bbc9815e5921c1
LautaroRiofrio <riofriolautaro@gmail.com>
2025-09-01 09:56:54 -0300

    refactor de cambiarEstado y cambiarOrden

--- src/app/reception/page.tsx
+++ src/app/reception/page.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { useState } from "react";
 
+import { useState, useEffect } from "react";
+
+// Componentes de Recepcion
 import Summary from "./components/summary/Summary";
-import { OrderModal } from "./components/orden";
+import OrdenModal from "./components/orden/ordenModal";
+
+// Interfaz
+
+import ordenesAleatorias from "../components/OrdenesAleatorias";
+
 
 const Reception = () => {
+
     const ordenes_creados: IOrder[] = [];
     const ordenes_pendientes: IOrder[] = [];
     const ordenes_preparados: IOrder[] = [];
@@ -12,35 +20,80 @@ const Reception = () => {
     const ordenes_entregados: IOrder[] = [];
     const ordenes_cancelados: IOrder[] = [];
 
+    ordenesAleatorias.map((orden) => {
+        if (orden.state == "Creados") {
+            ordenes_creados.push(orden)
+        } else if (orden.state == "Pendientes") {
+            ordenes_pendientes.push(orden)
+        } else if (orden.state == "Preparados") {
+            ordenes_preparados.push(orden)
+        } else if (orden.state == "En camino") {
+            ordenes_enCamino.push(orden)
+        } else if (orden.state == "Cancelado") {
+            ordenes_cancelados.push(orden)
+        } else if (orden.state == "Entregado") {
+            ordenes_entregados.push(orden)
+        }
+    });
+
     const [ordenActiva, setOrdenActiva] = useState<IOrder | null>(null);
     const [estadoOrden, setEstadoOrden] = useState<ORDER_UI_STATE>("default");
 
-    const cambiarEstado = (nuevoEstado: ORDER_UI_STATE) => {
-        if (estadoOrden == "nueva" && (nuevoEstado == "ver" || nuevoEstado == "default")) {
-            const confirmar = window.confirm("Hay cambios sin guardar");
-            // retorna false tanto para la funcion cambiarOrden como tambien para cortar la ejecucion y evitar los proximos condicionales
-            if (!confirmar) return false;
+    const puedeCambiarEstado = (nuevoEstado: ORDER_UI_STATE): boolean => {
+        if (estadoOrden === "nueva" && (nuevoEstado === "ver" || nuevoEstado === "default")) {
+            return window.confirm("Hay cambios sin guardar");
         }
 
-        if (estadoOrden == "editar" && (nuevoEstado == "ver" || nuevoEstado == "nueva")) {
-            const confirmar = window.confirm("Hay cambios sin guardar");
-            if (!confirmar) return false;
+        if (estadoOrden === "editar" && (nuevoEstado === "ver" || nuevoEstado === "nueva")) {
+            return window.confirm("Hay cambios sin guardar");
         }
-        setEstadoOrden(nuevoEstado);
+
         return true;
-    }
+    };
+
+
+    const cambiarEstado = (nuevoEstado: ORDER_UI_STATE) => {
+        if (puedeCambiarEstado(nuevoEstado)) setEstadoOrden(nuevoEstado);
+    };
 
     const cambiarOrden = (nuevaOrden: IOrder) => {
-        // No uso useEffect porque no me detecta cuando cambia a la misma orden y eso provoca que "cambiarEstado" no se active. 
         if (ordenActiva == null) {
             setOrdenActiva(nuevaOrden);
             cambiarEstado("ver");
         } else {
-            let permitido = cambiarEstado("ver");
-            console.log("Hola");
-            if (permitido) setOrdenActiva(nuevaOrden);
+            if (puedeCambiarEstado("ver")) {
+                setOrdenActiva(nuevaOrden);
+                cambiarEstado("ver");
+            }
         }
-    }
+    };
+    // const cambiarEstado = (nuevoEstado: estadosDeOrden) => {
+    //     if (estadoOrden == "nueva" && (nuevoEstado == "ver" || nuevoEstado == "default")) {
+    //         const confirmar = window.confirm("Hay cambios sin guardar");
+    //         // retorna false tanto para la funcion cambiarOrden como tambien para cortar la ejecucion y evitar los proximos condicionales
+    //         if (!confirmar) return false;
+    //     }
+
+    //     if (estadoOrden == "editar" && (nuevoEstado == "ver" || nuevoEstado == "nueva")) {
+    //         const confirmar = window.confirm("Hay cambios sin guardar");
+    //         if (!confirmar) return false;
+    //     }
+    //     setEstadoOrden(nuevoEstado);
+    //     return true;
+    // }
+
+    // const cambiarOrden = (nuevaOrden: IOrder) => {
+    //     // No uso useEffect porque no me detecta cuando cambia a la misma orden y eso provoca que "cambiarEstado" no se active. 
+    //     if (ordenActiva == null) {
+    //         setOrdenActiva(nuevaOrden);
+    //         cambiarEstado("ver");
+    //     } else {
+    //         let permitido = cambiarEstado("ver");
+    //         console.log("Hola");
+    //         if (permitido) setOrdenActiva(nuevaOrden);
+    //     }
+    // }
+
 
     return (
         <div className="grid grid-cols-7 gap-6 h-full">
@@ -56,7 +109,7 @@ const Reception = () => {
                 />
             </div>
             <div className="col-span-3 flex flex-col">
-                <OrderModal orden={ordenActiva} estado={estadoOrden} setEstado={cambiarEstado} />
+                <OrdenModal orden={ordenActiva} estado={estadoOrden} setEstado={cambiarEstado} />
             </div>
         </div>
     );
