3ab45f1f0ecf80dc3882eb057a9f246bc88497aa
tomas0011 <tomasbenjamin117@gmail.com>
2025-10-05 13:22:28 -0300

    Add dynamic time display to Navbar and OrdenDefault components

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,19 +1,39 @@
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    const updateTime = () => {
+      const formatter = new Intl.DateTimeFormat('en-US', {
+        timeZone: 'America/Buenos_Aires',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      });
+      setCurrentTime(formatter.format(new Date()));
+    };
+
+    updateTime();
+    const interval = setInterval(updateTime, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-<div className="flex items-center justify-between p-2">
-  <h1>Pipí Cucú</h1>
-  <div className="flex items-center gap-4">
-    <ul className="flex gap-4 text-[12px] sm:text-sm">
-      <li><Link href="/reception">Recepción</Link></li>
-      <li><Link href="/kitchen">Cocina</Link></li>
-      <li><Link href="/delivery">Entrega</Link></li>
-      <li><Link href="/products">Productos</Link></li>
-    </ul> 
-    <span className="font-poppins text-brand font-black text-xl">19:32</span>
-  </div>
-</div>
+    <div className="flex items-center justify-between p-2">
+      <h1>Pipí Cucú</h1>
+      <div className="flex items-center gap-4">
+        <ul className="flex gap-4 text-[12px] sm:text-sm">
+          <li><Link href="/reception">Recepción</Link></li>
+          <li><Link href="/kitchen">Cocina</Link></li>
+          <li><Link href="/delivery">Entrega</Link></li>
+          <li><Link href="/products">Productos</Link></li>
+        </ul>
+        <span className="font-poppins text-brand font-black text-xl">{currentTime}</span>
+      </div>
+    </div>
   );
 }
 
--- src/app/reception/components/orden/ordenDefault/index.tsx
+++ src/app/reception/components/orden/ordenDefault/index.tsx
@@ -1,14 +1,33 @@
-const OrdenDefault = () => {
+import { useEffect, useState } from "react";
 
+const OrdenDefault = () => {
+    const [currentTime, setCurrentTime] = useState('');
+    
+      useEffect(() => {
+        const updateTime = () => {
+          const formatter = new Intl.DateTimeFormat('en-US', {
+            timeZone: 'America/Buenos_Aires',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+          });
+          setCurrentTime(formatter.format(new Date()));
+        };
+    
+        updateTime();
+        const interval = setInterval(updateTime, 30000);
+    
+        return () => clearInterval(interval);
+      }, []);
 
     return (
         <div className="flex flex-col items-center justify-center text-center font-poppins text-primary">
             <span className="text-[150px] font-black opacity-10 w-full text-right pr-10">+</span>
             <div>
                 <div>
-                    <h1 className="text-5xl font-black">20:02</h1>
+                    <h1 className="text-5xl font-black">{currentTime}</h1>
                     <svg width="150" height="150" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.5-13h-1v6l5.25 3.15.75-1.23-4.5-2.67V7z" fill="currentColor"/>
+                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.5-13h-1v6l5.25 3.15.75-1.23-4.5-2.67V7z" fill="currentColor" />
                     </svg>
                 </div>
                 <span className="text-sm font-light">Nuevo pedido en 30&rsquo;</span>
